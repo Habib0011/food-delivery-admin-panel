b7f2310356342ae3707ac39eabee42327d78ed8e
unknown <habiburrahman5236@gmail.com>
2026-07-26 13:03:57 +0600

    Fix dynamic API URL with environment variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import SideBar from './components/SideBar/SideBar';
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Add from './pages/Add/Add';
 import List from './pages/List/List';
 import Order from './pages/Order/Order';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import { url } from './assets/assets.js';
+
 const App = () => {
-  const url = "https://food-delivery-backend-nykh.onrender.com/";
+
   return (
     <div>
       <ToastContainer></ToastContainer>
-      <Navbar ></Navbar>
+      <Navbar />
       <hr />
+
       <div className="app-content">
-        <SideBar></SideBar>
+        <SideBar />
+
         <Routes>
-          <Route path='/add' element={<Add url={url}></Add>}></Route>
-          <Route path='/list' element={<List url={url}></List>}></Route>
-          <Route path='/order' element={<Order url={url}></Order>}></Route>
+          <Route path="/" element={<Navigate to="/add" replace />} />
+          <Route path='/add' element={<Add url={url} />} />
+          <Route path='/list' element={<List url={url} />} />
+          <Route path='/order' element={<Order url={url} />} />
         </Routes>
+
       </div>
     </div>
   );
